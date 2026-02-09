cf9ea93f3b4013f06cffc332c547b592e68b78e1
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-09 17:22:43 -0300

    Entity User & REST

---
+++ src/main/java/com/enzima01/webservice/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.enzima01.webservice.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enzima01.webservice.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		User u1 = new User("1", "Enzo", "enzo@gmail.com");
+		User u2 = new User("2", "Maria", "maria@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(u1, u2));
+		return ResponseEntity.ok().body(list);
+	}
+}
