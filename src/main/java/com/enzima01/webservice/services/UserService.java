d6badcedc5f75084f2b662a93304be16308c9179
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-10 19:04:23 -0300

    Finding user by id

--- src/main/java/com/enzima01/webservice/services/UserService.java
+++ src/main/java/com/enzima01/webservice/services/UserService.java
@@ -1,20 +1,28 @@
 package com.enzima01.webservice.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enzima01.webservice.domain.User;
 import com.enzima01.webservice.repositories.UserRepository;
+import com.enzima01.webservice.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+
 }
