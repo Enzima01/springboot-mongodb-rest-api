01cf5e299ee86a6745d372ce79450643237723c1
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-18 16:24:00 -0300

    Endpoint to return a User's posts

--- src/main/java/com/enzima01/webservice/resources/UserResource.java
+++ src/main/java/com/enzima01/webservice/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.enzima01.webservice.domain.Post;
 import com.enzima01.webservice.domain.User;
 import com.enzima01.webservice.dto.UserDTO;
 import com.enzima01.webservice.services.UserService;
@@ -60,6 +61,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPost());
 	}
 
 }
